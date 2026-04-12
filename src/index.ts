21e9b6f52533f0056e0991422becabfab5e72023
Marcus Xavier <marcusxavierr123@gmail.com>
2026-04-12 18:33:07 -0300

    chore: make the port dynamic

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { app } from './app'
 
-app.listen({
-  port: 9999,
-  tls: {
-    cert: Bun.file('/home/marcus/Projects/courses/webrtc/192.168.3.141.pem'),
-    key: Bun.file('/home/marcus/Projects/courses/webrtc/192.168.3.141-key.pem'),
-  },
-})
+const port = Number(process.env.PORT) || 9999
+
+app.listen({ port })
 
 console.log(`Listening at :${app.server?.port}`)
